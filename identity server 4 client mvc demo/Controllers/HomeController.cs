3886239ede952c700b377833845f7b2d30bdf228
Kugan <kk.nesan@gmail.com>
2022-04-11 18:22:21 +0800

    Add Home/Weather action

--- identity server 4 client mvc demo/Controllers/HomeController.cs
+++ identity server 4 client mvc demo/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
 ﻿using identity_server_4_client_mvc_demo.Models;
+using identity_server_4_client_mvc_demo.Services;
+using IdentityModel.Client;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Diagnostics;
 
 namespace identity_server_4_client_mvc_demo.Controllers
@@ -7,10 +10,12 @@ namespace identity_server_4_client_mvc_demo.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ITokenService _tokenService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ITokenService tokenService)
         {
             _logger = logger;
+            _tokenService = tokenService;
         }
 
         public IActionResult Index()
@@ -21,6 +26,36 @@ namespace identity_server_4_client_mvc_demo.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        public async Task<IActionResult> Weather()
+        {
+            var data = new List<WeatherData>();
+
+            using (var client = new HttpClient())
+            {
+                var tokenResponse = await _tokenService.GetToken("weatherapi.read");
+
+                client.SetBearerToken(tokenResponse.AccessToken);
+
+
+
+                var result = await client
+                    .GetAsync("https://localhost:7245/weatherforecast");
+
+                if (result.IsSuccessStatusCode)
+                {
+                    var model = await result.Content.ReadAsStringAsync();
+
+                    data = JsonConvert.DeserializeObject<List<WeatherData>>(model);
+
+                    return View(data);
+                }
+                else
+                {
+                    throw new Exception("Unable to get content");
+                }
+            }
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
