f736e5621f8edcaf84f8c76a5e98b2aa3cb20fff
Kugan <kk.nesan@gmail.com>
2022-04-11 18:18:58 +0800

    Add ITokenService & TokenService
    1. added - GetToken() method.
    2. added - TokenService() constructor.

---
+++ identity server 4 client mvc demo/Services/ITokenService.cs
@@ -0,0 +1,9 @@
+﻿using IdentityModel.Client;
+
+namespace identity_server_4_client_mvc_demo.Services
+{
+    public interface ITokenService
+    {
+        Task<TokenResponse> GetToken(string scope);
+    }
+}
---
+++ identity server 4 client mvc demo/Services/IdentityServerSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace identity_server_4_client_mvc_demo.Services
+{
+    public class IdentityServerSettings
+    {
+        public string DiscoveryUrl { get; set; }
+
+        public string ClientName { get; set; }
+
+        public string ClientPassword { get; set; }
+
+        public bool UseHttps { get; set; }
+    }
+}
