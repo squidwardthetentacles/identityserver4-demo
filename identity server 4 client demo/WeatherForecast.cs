f16848ded656a53fc200d5a49b960a8303fde158
Kugan <kk.nesan@gmail.com>
2022-04-11 16:12:23 +0800

    Add identity server 4 frontend project

---
+++ identity server 4 client demo/WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace identity_server_4_client_demo;
+
+public class WeatherForecast
+{
+    public DateTime Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
