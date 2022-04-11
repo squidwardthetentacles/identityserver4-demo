35eeb8db0e2af2b63dd7cc8589a7f398c0a5e415
Kugan <kk.nesan@gmail.com>
2022-04-11 16:33:32 +0800

    Add authorization for api

--- identity server 4 client demo/Controllers/WeatherForecastController.cs
+++ identity server 4 client demo/Controllers/WeatherForecastController.cs
@@ -1,9 +1,11 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace identity_server_4_client_demo.Controllers;
 
 [ApiController]
 [Route("[controller]")]
+[Authorize]
 public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
