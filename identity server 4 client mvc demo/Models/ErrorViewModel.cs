a6baf3665400cf65a4c14d0197675c7b1502c23e
Kugan <kk.nesan@gmail.com>
2022-04-11 16:34:39 +0800

    Add identity server 4 frontend mvc project

---
+++ identity server 4 client mvc demo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace identity_server_4_client_mvc_demo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
