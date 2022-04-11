761f36a3ad3acf9f65861d2968300714325399f1
Kugan <kk.nesan@gmail.com>
2022-04-11 18:23:50 +0800

    Add WeatherData model

---
+++ identity server 4 client mvc demo/Models/WeatherData.cs
@@ -0,0 +1,13 @@
+﻿namespace identity_server_4_client_mvc_demo.Models
+{
+    public class WeatherData
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
